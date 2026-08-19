8a405b1fd2a3e4c97282065d633e431cc2b8f585
linhtran1697 <linhtran.1697.lt@gmail.com>
2026-08-19 11:41:14 +0900

    Add files via upload

---
+++ SS1_ex3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class SS1_ex3 {
+    public static void main (String [] args){
+        // Khai báo biến
+        double radius;
+        double area;
+        // Tạo đối tượng Scanner để nhập dữ liệu từ người dùng
+        Scanner scanner = new Scanner (System.in);
+        // Yêu cầu người dùng nhập bán kính
+        System.out.print ("Nhập bán kính của hình tròn:");
+        radius = scanner.nextDouble();
+        // Tính diện tích hình tròn
+        area = Math.PI * radius * radius;
+        // Hiển thị kết quả ra màn hình
+        System.out.printf("Diện tích hình tròn có bán kính là %.2f là: %.2f%n", radius, area);
+        // Đóng scanner
+        scanner.close ();
+    }
+}
