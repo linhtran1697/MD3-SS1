0d90952e6d525442093c8c17fc6a4080c230be4b
linhtran1697 <linhtran.1697.lt@gmail.com>
2026-08-19 11:02:52 +0900

    Add files via upload

---
+++ MD3_ss1_ex2.java
@@ -0,0 +1,9 @@
+public class MD3_ss1_ex2 {
+    public static void main (String [] args) {
+        // khai báo và khởi tạo thông tin cá nhân
+        String name = " Trần Thị Thuỳ Linh";
+        int age = 29;
+        String address = "Nghệ An";
+        System.out.printf("Tên tôi là %s, tôi %d tuổi, hiện tại tôi đang làm việc tại Nhật. Quê ở %s.%n", name,age,address);
+    }
+}
