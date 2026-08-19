974f469e02a3d25fc01875d53560b78577145243
linhtran1697 <linhtran.1697.lt@gmail.com>
2026-08-19 11:53:12 +0900

    Add files via upload

---
+++ MD3_SS1_ex4.java
@@ -0,0 +1,26 @@
+public class MD3_SS1_ex4 {
+    public static void main (String [] args) {
+        // Khai báo và khởi tạo hai số biến nguyên
+        int a = 20;
+        int b = 5;
+        // Thực hiện các phép toán
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        int quotient = a / b;
+        int remainder = a % b;
+        // In kết quả ra màn hình
+        System.out.printf ("Giá trị a: %d%n", a);
+        System.out.printf ("Giá trị b: %d%n", b);
+        System.out.printf ("Tổng của a và b: %d%n", sum);
+        System.out.printf ("Hiệu quả a và b: %d%n", difference);
+        System.out.printf ("Tích quả a và b: %d%n", product);
+        System.out.printf ("Thương quả a và b: %d%n", quotient);
+        System.out.printf ("Phần dư khi chia a cho b: %d%n", remainder);
+
+
+
+
+
+    }
+}
